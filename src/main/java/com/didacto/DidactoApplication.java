6d109f3546474c41fd4291464a95d598b283ecee
yoonho0922 <yoonho5684@gmail.com>
2024-03-14 19:31:15 +0900

    init

---
+++ src/main/java/com/didacto/DidactoApplication.java
@@ -0,0 +1,13 @@
+package com.didacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidactoApplication.class, args);
+	}
+
+}
