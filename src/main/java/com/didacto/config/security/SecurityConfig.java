a6cb2ed7a3ce69bc016782340c6ef13364410971
Lwonbin <lwb9036@gmail.com>
2024-03-26 16:35:50 +0900

    feat : @ securityConfig에nableMethodSecurity 적용

--- src/main/java/com/didacto/config/security/SecurityConfig.java
+++ src/main/java/com/didacto/config/security/SecurityConfig.java
@@ -9,6 +9,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +27,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 
 //TODO :
 // 1. JWT 인증 및 인가에 대한 설정을 추가한다.
