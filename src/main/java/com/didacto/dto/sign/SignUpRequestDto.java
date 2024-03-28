fb0a7df02c8530482c0dc9312adbb3f376f9bc7f
Lwonbin <lwb9036@gmail.com>
2024-03-29 00:48:52 +0900

    feat : SignUpRequestDto 패턴, 이메일 예외처리 추가

--- src/main/java/com/didacto/dto/sign/SignUpRequestDto.java
+++ src/main/java/com/didacto/dto/sign/SignUpRequestDto.java
@@ -1,7 +1,9 @@
 package com.didacto.dto.sign;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,11 +18,12 @@ public class SignUpRequestDto {
 
     @NotBlank(message = "이메일 입력해주세요.")
     @Schema(description = "이메일", example = "abc123@naver.com")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
     @Schema(description = "비밀번호", example = "abcasdj456789!!")
-//    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}", message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     private String password;
 
     @NotBlank(message = "사용자 이름을 입력해주세요.")
