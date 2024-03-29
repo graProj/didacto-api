930796f5ddb6afc87ecfbcbb7d5ccfeabf254b93
Lwonbin <lwb9036@gmail.com>
2024-03-29 11:46:46 +0900

    feat : add AcessException Handler(#8)

--- src/main/java/com/didacto/config/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/didacto/config/jwt/JwtAccessDeniedHandler.java
@@ -1,20 +1,38 @@
 package com.didacto.config.jwt;
 
+import com.didacto.common.ErrorDefineCode;
+import com.didacto.common.response.CommonError;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
+@Slf4j(topic = "FORBIDDEN_EXCEPTION_HANDLER")
 @Component
+@AllArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
+    private final ObjectMapper objectMapper;
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        // 필요한 권한이 없이 접근하려 할때 403
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("No Authorities", accessDeniedException);
+        CommonError errorResponseDto = new CommonError(ErrorDefineCode.AUTHENTICATE_FAIL.getCode(), LocalDateTime.now());
+        String responseBody = objectMapper.writeValueAsString(errorResponseDto);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(responseBody);
     }
 }
--- src/main/java/com/didacto/config/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/didacto/config/jwt/JwtAuthenticationEntryPoint.java
@@ -1,19 +1,35 @@
 package com.didacto.config.jwt;
 
+import com.didacto.common.ErrorDefineCode;
+import com.didacto.common.response.CommonError;
+import com.didacto.common.response.CommonResponse;
+import com.didacto.config.exception.custom.exception.ForbiddenException403;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
+@AllArgsConstructor
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private final ObjectMapper objectMapper;
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        CommonError errorResponseDto = new CommonError(ErrorDefineCode.AUTHENTICATE_FAIL.getCode(), LocalDateTime.now());
+        String responseBody = objectMapper.writeValueAsString(errorResponseDto);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(responseBody);
     }
 }
