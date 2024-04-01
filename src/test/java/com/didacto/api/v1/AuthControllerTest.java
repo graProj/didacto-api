cd32bec4b8742d7cac18f19ab7a6e2a69a546028
Lwonbin <lwb9036@gmail.com>
2024-04-01 12:25:28 +0900

    feat 회원 : Proceeding signup test

---
+++ src/test/java/com/didacto/api/v1/AuthControllerTest.java
@@ -0,0 +1,67 @@
+package com.didacto.api.v1;
+
+import com.didacto.config.security.jwt.TokenProvider;
+import com.didacto.dto.auth.SignUpRequest;
+import com.didacto.repository.member.MemberRepository;
+import com.didacto.service.auth.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.MediaType;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Transactional
+@DisplayName("Auth Controller")
+@ExtendWith(MockitoExtension.class)
+class AuthControllerTest {
+    @InjectMocks
+    AuthController authController;
+    @Mock
+    AuthService authService;
+    MockMvc mockMvc;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void beforeEach() {
+        mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
+    }
+
+    @Test
+    public void 회원가입_테스트() throws Exception{
+
+        //given
+        SignUpRequest req = new SignUpRequest("gildong@naver.com", "gildong123!!","홍길동","20000621","USER");
+
+        //when
+        mockMvc.perform(
+                post("/api/v1/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(req))
+        ).andExpect(status().isCreated());
+
+        //then
+        verify(authService).signup(req);
+    }
+//
+//    @Test
+//    void signIn() {
+//    }
+}
