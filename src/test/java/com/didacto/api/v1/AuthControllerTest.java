4150041ea0d56d3ed24edd03915bf751ed5abbb5
Lwonbin <lwb9036@gmail.com>
2024-04-03 11:15:53 +0900

    refactor : change method name

--- src/test/java/com/didacto/api/v1/AuthControllerTest.java
+++ src/test/java/com/didacto/api/v1/AuthControllerTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
