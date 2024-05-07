17a5675496fb238637b519111bcb8d6e842e60d5
Lwonbin <lwb9036@gmail.com>
2024-05-07 19:26:11 +0900

    add : build.gradle ãsettings test h22(#35)

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE member (
+    member_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    birth DATETIME,
+    role ENUM('USER'),
+    deleted BOOLEAN,
+    created_time DATETIME NOT NULL,
+    modified_time DATETIME,
+);
