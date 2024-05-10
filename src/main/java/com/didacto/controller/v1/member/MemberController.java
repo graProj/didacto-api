140a40095d12dd8ddc07b1b93484cf8e840a1459
Lwonbin <lwb9036@gmail.com>
2024-05-10 12:44:40 +0900

    feat : MemberController add PreAuthorize and Sagger description add Get Mothod(#35)

--- src/main/java/com/didacto/controller/v1/member/MemberController.java
+++ src/main/java/com/didacto/controller/v1/member/MemberController.java
@@ -1,6 +1,7 @@
 package com.didacto.controller.v1.member;
 
 import com.didacto.common.response.CommonResponse;
+import com.didacto.config.security.AuthConstant;
 import com.didacto.config.security.SecurityUtil;
 import com.didacto.dto.member.MemberModificationRequest;
 import com.didacto.dto.member.MemberResponse;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,8 +27,7 @@ import java.util.List;
 public class MemberController {
 
     private final MemberService memberService;
-
-    @Operation(summary = "MEMBER_01 : 회원 전체 조회 API", description = "전체 회원을 조회한다.")
+    @Operation(summary = "MEMBER_01 : 회원 전체 조회 API", description = "전체 회원을 조회한다. (관리자용)")
     @GetMapping("/members")
     @ResponseStatus(HttpStatus.OK)
     public CommonResponse findAllMembers(){
@@ -34,7 +35,7 @@ public class MemberController {
         return new CommonResponse<>(true, HttpStatus.OK, "회원 조회에 성공했습니다.", result);
     }
 
-    @Operation(summary = "MEMBER_02 : 회원 개별 조회 API", description = "개별 회원을 조회한다.")
+    @Operation(summary = "MEMBER_02 : 회원 개별 조회 API", description = "개별 회원을 조회한다. (관리자용)")
     @GetMapping("/members/{id}")
     public CommonResponse findMember(@PathVariable("id") Long id) {
         MemberResponse result= memberService.query(id);
@@ -46,6 +47,7 @@ public class MemberController {
 
 
 
+    @PreAuthorize(AuthConstant.AUTH_ALL)
     @Operation(summary = "MEMBER_03 : 회원 정보 수정 API", description = "회원 정보를 수정한다.")
     @PutMapping("/members")
     public CommonResponse editMemberInfo(@RequestBody MemberModificationRequest memberEditRequest){
@@ -59,6 +61,7 @@ public class MemberController {
     }
 
 
+    @PreAuthorize(AuthConstant.AUTH_ALL)
     @Operation(summary = "MEMBER_04 : 회원 탈퇴 API", description = "회원을 탈퇴시킨다.")
     @DeleteMapping("/members")
     public CommonResponse deleteMemberInfo() {
