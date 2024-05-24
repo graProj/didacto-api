ebfa9919100e81a2beaf5a824eee5b4224d5f1c2
Jaehwi-So <sjh9708@gmail.com>
2024-05-25 00:55:21 +0900

    fix : add Lecture object Builder

--- src/main/java/com/didacto/dto/lecture/LectureModificationRequest.java
+++ src/main/java/com/didacto/dto/lecture/LectureModificationRequest.java
@@ -1,6 +1,7 @@
 package com.didacto.dto.lecture;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,4 +13,10 @@ public class LectureModificationRequest {
 
     @Schema(example = "수정된 강의 제목")
     private String title;
+
+    @Builder
+    public LectureModificationRequest(Long lectureId, String title) {
+        this.lectureId = lectureId;
+        this.title = title;
+    }
 }
