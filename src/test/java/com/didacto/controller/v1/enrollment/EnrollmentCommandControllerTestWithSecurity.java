63722aaaa80848027f9ac6820e15f8d8093a6737
Jaehwi-So <sjh9708@gmail.com>
2024-08-26 09:16:41 +0900

    test : EnrollmentCommandController 테스트 케이스 작성 : 강의 초대 요청 (#61)

--- src/test/java/com/didacto/controller/v1/enrollment/EnrollmentCommandControllerTestWithSecurity.java
+++ src/test/java/com/didacto/controller/v1/enrollment/EnrollmentCommandControllerTestWithSecurity.java
@@ -11,16 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.MockedStatic;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mockStatic;
@@ -29,16 +28,26 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(controllers = EnrollmentCommandController.class)
-class EnrollmentCommandControllerTest {
+//@WebMvcTest(controllers = EnrollmentCommandController.class)
+@SpringBootTest
+class EnrollmentCommandControllerTestWithSecurity {
 
-    @Autowired
-    private MockMvc mockMvc;
     @MockBean
     private EnrollmentCommandService enrollmentService;
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
 
     @DisplayName("학생은 강의 초대 요청을 보낼 수 있다.")
@@ -62,6 +71,29 @@ class EnrollmentCommandControllerTest {
                     .andExpect(status().isOk());
         }
     }
+
+    @DisplayName("교수자는 강의 초대 요청을 할 수 없다.")
+    @WithMockUser(username = "TUTOR", roles = {AuthConstant.ADMIN})
+    @Test
+    void createRequestFromTutor() throws Exception {
+
+        //given
+        EnrollmentRequest request = new EnrollmentRequest(5L);
+        BDDMockito.given(enrollmentService.requestEnrollment(anyLong(), anyLong())).willReturn(1L);
+
+
+        try(MockedStatic<SecurityUtil> mSecurityUtil = mockStatic(SecurityUtil.class)) {
+            BDDMockito.given(SecurityUtil.getCurrentMemberId()).willReturn(2L);
+
+            //when, then
+            mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/enrollment").with(csrf())
+                            .content(objectMapper.writeValueAsString(request))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andExpect(status().isForbidden());
+        }
+    }
+
 
 
 
--- src/test/java/com/didacto/controller/v1/enrollment/EnrollmentQueryControllerTest.java
+++ src/test/java/com/didacto/controller/v1/enrollment/EnrollmentQueryControllerTest.java
@@ -1,9 +1,12 @@
 package com.didacto.controller.v1.enrollment;
 
+import com.didacto.config.security.AuthConstant;
+import com.didacto.config.security.SecurityUtil;
 import com.didacto.dto.enrollment.EnrollmentResponse;
 import com.didacto.service.enrollment.EnrollmentQueryService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,7 +15,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,14 +25,14 @@ class EnrollmentQueryControllerTest {
     @MockBean
     private EnrollmentQueryService enrollmentQueryService;
 
-    @DisplayName("학생은 특정 ID의 초대 정보를 조회할 수 있다.")
-    @WithMockUser(username = "STUDENT", roles = {"COMMON"})
+    @DisplayName("특정 ID의 초대 정보를 조회할 수 있다.")
+    @WithMockUser(username = "STUDENT", roles = {"USER"})
     @Test
     void queryEnrollmentByIdFromStudent() throws Exception {
 
         //given
         EnrollmentResponse enrollment = new EnrollmentResponse();
-        when(enrollmentQueryService.getEnrollmentById(1L)).thenReturn(enrollment);
+        BDDMockito.given(enrollmentQueryService.getEnrollmentById(1L)).willReturn(enrollment);
 
         //when, then
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/enrollment/1"))
@@ -42,22 +44,4 @@ class EnrollmentQueryControllerTest {
 
     }
 
-    @DisplayName("교수자는 특정 ID의 초대 정보를 조회할 수 있다.")
-    @WithMockUser(username = "TUTOR", roles = {"ADMIN"})
-    @Test
-    void queryEnrollmentByIdFromAdmin() throws Exception {
-
-        //given
-        EnrollmentResponse enrollment = new EnrollmentResponse();
-        when(enrollmentQueryService.getEnrollmentById(1L)).thenReturn(enrollment);
-
-        //when, then
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/enrollment/1"))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.success").value(true))
-                .andExpect(jsonPath("$.status").value("OK"))
-                .andExpect(jsonPath("$.message").value("강의 등록 요청 데이터를 조회하였습니다"));
-
-    }
 }
